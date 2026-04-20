a149aa8d025b775618bf0e8d29bc04a0ac5f710c
lakshita198 <lakshita.rakhecha@gmail.com>
2026-04-20 22:29:39 +0530

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,10 @@
+package com.example.contact;
+
+public class Contact {
+    String name, mobile;
+
+    public Contact(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+}
